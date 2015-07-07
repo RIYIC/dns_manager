c95b462782439d0797076b8f9f1304292c96324a
J. Gomez <alambike@gmail.com>
2015-07-07 08:55:31 +0200

    Agregado modelo de usuario, e asociado o modelo de dominios a usuarios

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,10 @@
 class Domain < ActiveRecord::Base
     has_many :records,dependent: :destroy
 
+    belongs_to :user
+
     validates :name, presence: true, uniqueness: true
+
+    validates :user_id, presence: true
 
 end
